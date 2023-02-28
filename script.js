b9c85a88fdabae67a01d6349cf9ce084d34098a8
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-02-28 14:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-//your code here
-//your code here
-function fun(even) {
-	even.preventDefault();
-var inp=document.querySelector('input').value;
-var arr=inp.split(",");
-	arr.sort(function(a,b){return a-b});
+function calculateMinCost(event) {
+	// event.preventDefault();
+	var inputEle = document.querySelector('input').value;
+	// alert(inputEle)
+	var arr = inputEle.split(',');
+	arr.sort(function(a,b){
+		return a-b;
+	});
 
-	
-	let cost=0;
-	while (arr.length>1) {
-	let curr=Number(arr[0])+Number(arr[1]);
-	arr.splice(0,2);
-	arr.push(curr);
-		cost+=curr;
-		arr.sort(function(a,b){return a-b});
+	var cost = 0;
+	while(arr.length>1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost+=res;
+		arr.shift();
+		arr.shift();
+		arr.sort(function(a,b){
+		return a-b;
+	});
 	}
-	var ans=document.getElementById('result').textContent=cost;
-}
+
+	document.getElementById('result').textContent = cost
+
+}  
